997caf7953888cf944fe9bd3c98b36a5cd025317
Smxkez <sandu.albert.alexandru@gmail.com>
2022-09-01 13:28:09 +0300

    Initial Commit

---
+++ src/utils/constants.js
@@ -0,0 +1,37 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCuAXFkgsw1L7xaCfnd5JJOw';
+export const demoVideoUrl = '/video/dQw4w9WgXcQ';
+export const demoChannelTitle = 'Rick Astley';
+export const demoVideoTitle = 'Rick Astley - Never Gonna Give You Up (Official Music Video)';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
